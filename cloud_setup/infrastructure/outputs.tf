b15ec7ef0e4165291dd27563b640beee062a5d82
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2024-02-07 14:10:59 +0100

    cleanup dirs

--- cloud_setup/infrastructure/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "postgresql_instance_ip" {
-  value = google_compute_instance.postgres_instance.network_interface[0].access_config[0].nat_ip
-}
-
-output "hammerdb_instance_ip" {
-  value = google_compute_instance.hammerdb_instance.network_interface[0].access_config[0].nat_ip
-}
