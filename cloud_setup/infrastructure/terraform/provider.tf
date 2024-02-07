171138efb4422af47414508ad57494fd9bacd10a
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2024-02-07 14:11:14 +0100

    cleanup but refactoring needed

---
+++ cloud_setup/infrastructure/terraform/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project = "ssws23"
+  region = "eu-west1"
+  zone = "europe-west1-b" 
+
+}
---
+++ cloud_setup/infrastructure/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {
+  description = "GCP instance type"
+  type        = string
+  default     = "n1-standard-2"
+}
+
+variable "public_key_path" {
+  description = "Path to the public SSH key on the local machine"
+  type        = string
+}
