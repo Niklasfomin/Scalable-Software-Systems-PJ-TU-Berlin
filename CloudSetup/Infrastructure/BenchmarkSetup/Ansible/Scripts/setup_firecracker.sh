348818667acf96318057e5929dae87dc933d07d8
Niklasfomin <niklas.fomin@campus.tu-berlin.de>
2024-02-17 23:09:14 +0100

    firecracker setup works, pgsql WIP

--- CloudSetup/Infrastructure/BenchmarkSetup/Ansible/Scripts/setup_firecracker.sh
+++ CloudSetup/Infrastructure/BenchmarkSetup/Ansible/Scripts/setup_firecracker.sh
@@ -11,41 +11,40 @@ print_message() {
 sudo apt install -yqq tmux > /dev/null
 
 # Setup networking on the host
-#sudo iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
-#sudo iptables -A FORWARD -i ens4 -o tap-fc-dev -m state --state RELATED,ESTABLISHED -j ACCEPT
-#sudo iptables -A FORWARD -o ens4 -i tap-fc-dev -j ACCEPT
-
-sleep 10
+sudo iptables -t nat -A POSTROUTING -o ens4 -j MASQUERADE
+sudo iptables -A FORWARD -i ens4 -o tap-fc-dev -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -o ens4 -i tap-fc-dev -j ACCEPT
 
 # Download installation wizard for aws firecracker
 git clone https://github.com/Schachte/Firecracker-VM-Wizard.git
 
 cd Firecracker-VM-Wizard
 
-{
-    echo 5
-    for i in {1..5}; do
-        echo
-    done 
-    echo 7
-} | sudo ./wizard
+# Setup kernel, filesystem and vm config
+sudo bash -c 'sudo bash wizard <<EOF
+1
+2
+3
+EOF
+'
 
+sudo bash -c 'sudo bash wizard <<EOF
+4
+EOF
+'
 
-sleep 3
+# Setup tap device and configure vm network
+{     echo 5;     for i in {1..5}; do         echo;     done;     echo 8;} | sudo bash wizard
 
-export CONFIG_LOCATION=/firecracker/configs
-sudo firecracker --no-api --config-file $CONFIG_LOCATION/alpine-config.json 
+tmux has-session -t FirecrackerVM 2>/dev/null && tmux kill-session -t FirecrackerVM
 
-#cd ..
-#cd Firecracker-VM-Wizard
+tmux new-session -d -s FirecrackerVM -n Setup 
+tmux split-window -h -t FirecrackerVM:Setup 'echo "Starting VM..."; sudo bash wizard <<EOF
+6
+EOF
+; read -p "Press enter to close..."' 
 
-# Run login shell in different session because useless
-#tmux new-session -d -s Firecracker "echo 6 | sudo ./wizard"
-
-# Connect to MicroVM
-#sudo ssh -i ~/.ssh/hacker alpine@172.16.0.2
-
-# Setup networking
-#sudo ip addr add 172.16.0.2/24 dev eth0
-#sudo ip link set eth0 up
-#sudo ip route add default via 172.16.0.1
+tmux resize-pane -t FirecrackerVM:Setup -L 20
+tmux select-pane -t FirecrackerVM:Setup.0
+tmux send-keys -t FirecrackerVM:Setup.0 'read -p "SSH now? (Press enter to continue...)"; clear; ssh -i ~/.ssh/hacker alpine@172.16.0.2' C-m
+tmux attach-session -t FirecrackerVM
